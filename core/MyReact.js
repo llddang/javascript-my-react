43cbbd5a85fdb796697d6db260f00ad40a17d926
llddang <bodyness@naver.com>
2025-02-03 10:22:37 +0900

    feat: 리렌더링을 위해 내부 render 함수 구현

--- core/MyReact.js
+++ core/MyReact.js
@@ -1,12 +1,17 @@
 export default function MyReact() {
   let _root = null;
+  let _rootComponent = null;
 
   function createRoot(root) {
     _root = root;
     return { render };
   }
   function render(Component) {
-    const comp = Component();
+    _rootComponent = Component;
+    _render();
+  }
+  function _render() {
+    const comp = _rootComponent();
     _root.innerHTML = comp;
   }
 
